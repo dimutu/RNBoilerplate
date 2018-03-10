67504d358d88bfdd75dd9c1668ddf5c951e0a107
Dimutu Kulawardana <dev@dimutu.com>
2018-03-10 17:57:40 +1100

    reset project generated from RN command line

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import NavigationReducer from './NavigationReducer';
-import DataReducer from './DataReducer';
-
-export default combineReducers({
-  nav: NavigationReducer,
-  data: DataReducer,
-});
